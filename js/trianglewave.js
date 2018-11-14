9d700ec6b3d86c89e8b80e58b866cd48f300caef
cervangirard <cervan@thinkr.fr>
2018-11-14 10:24:40 +0100

    changement photo

--- js/trianglewave.js
+++ js/trianglewave.js
@@ -142,15 +142,15 @@ function getImg () {
   var ratio = vp.ratio;
   var width = vp.width;
   if (width > 1080) {
-    return 'images/agrotech.jpg';
+    return 'images/paris2.jpg';
   } else if (width > 720 && ratio > 1) {
-    return 'images/agrotech.jpg';
+    return 'images/paris2.jpg';
   } else if (width > 720 && ratio == 1) {
-    return 'images/agrotech.jpg';
+    return 'images/paris2.jpg';
   } else if (ratio > 1) {
-    return 'images/agrotech.jpg';
+    return 'images/paris2.jpg';
   } else {
-    return 'images/agrotech.jpg';
+    return 'images/paris2.jpg';
   }
 }
 
